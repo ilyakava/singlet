cd69641f2286be3cf0769c7772bd2f153fcda1e4
Ilya Kavalerov <ilya@artsymail.com>
2014-05-21 23:42:56 -0400

    Figure out how to remove weird annotation

--- converter.scala
+++ converter.scala
@@ -12,38 +12,37 @@ import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
 
 import util.Random.nextInt
 
+// a request to resize an image
 case class ImageResize(service: PooledGMService, srcPath: Path, maxLongSide: Int)
+// a new image that has completed processing
+case class AdditionalImage(newSrcPath: Path)
 
 class ImageProcessor extends Actor with ActorLogging {
-  // This annotation is annoying, but it does not prevent other cases from matching
-  // I don't 100% understand the workaround: http://stackoverflow.com/questions/12869251/the-argument-types-of-an-anonymous-function-must-be-fully-known-sls-8-5
-  def receive = {(req: ImageResize) =>
-    req match {
-      case ImageResize(service: PooledGMService, srcPath: Path, maxLongSide: Int) =>
-        try {
-          println("starting")
-          val thumbnailPath = Files.createTempFile("thumbnail", ".jpg").toAbsolutePath()
-          val imgIn: BufferedImage = ImageIO.read(srcPath.toFile())
+  def receive = {
+    case ImageResize(service: PooledGMService, srcPath: Path, maxLongSide: Int) =>
+      try {
+        println("starting")
+        val thumbnailPath = Files.createTempFile("thumbnail", ".jpg").toAbsolutePath()
+        val imgIn: BufferedImage = ImageIO.read(srcPath.toFile())
 
-          val scale = if (imgIn.getWidth() >= imgIn.getHeight()) {
-            // horizontal or square image
-            Math.min(maxLongSide, imgIn.getWidth()) / imgIn.getWidth().toDouble
-          } else {
-            // vertical image
-            Math.min(maxLongSide, imgIn.getHeight()) / imgIn.getHeight().toDouble
-          }
+        val scale = if (imgIn.getWidth() >= imgIn.getHeight()) {
+          // horizontal or square image
+          Math.min(maxLongSide, imgIn.getWidth()) / imgIn.getWidth().toDouble
+        } else {
+          // vertical image
+          Math.min(maxLongSide, imgIn.getHeight()) / imgIn.getHeight().toDouble
+        }
 
-          service.execute("convert",
-                     srcPath.toString(),
-                     "-resize", Math.round(100 * scale) + "%",
-                     thumbnailPath.toString())
+        service.execute("convert",
+                   srcPath.toString(),
+                   "-resize", Math.round(100 * scale) + "%",
+                   thumbnailPath.toString())
 
-          log.info("Thumbnail was saved to {}", thumbnailPath)
-          println(thumbnailPath)
-        } catch { case ex: Exception =>
-          println(ex.getMessage())
-            throw new IllegalStateException(ex.getMessage(), ex)
-      }
+        log.info("Thumbnail was saved to {}", thumbnailPath)
+        println(thumbnailPath)
+      } catch { case ex: Exception =>
+        println(ex.getMessage())
+          throw new IllegalStateException(ex.getMessage(), ex)
     }
   }
 }
